16a0fd438758f733a1536e3db5b1b09bf029aa06
Volodymyr Golubyev <vlad@Volodymyrs-MacBook-Pro.local>
2023-06-08 17:09:02 -0600

    Init

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import AccountMenu from './components/AccountMenu.js';
+import BasicForm from './components/BasicForm.js'
+
+import { useNavigate } from 'react-router-dom';
+
+export default function App() {
+	const [openForm, setOpenForm] = React.useState(false);
+	const handleOnClick = (page) => {
+		console.log('open');
+		if(!openForm) { 
+			setOpenForm(true)
+		} else {
+			setOpenForm(false)
+		}
+	};
+  return (
+    <Container maxWidth="sm">
+	<AccountMenu onClick={handleOnClick} />
+	{openForm && <BasicForm />}
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          React Skills test
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import AccountMenu from './components/AccountMenu.js';
+import BasicForm from './components/BasicForm.js'
+
+export default function App() {
+	const [openForm, setOpenForm] = React.useState(false);
+	const handleOnClick = (page) => {
+		console.log('open');
+		if(!openForm) { 
+			setOpenForm(true)
+		} else {
+			setOpenForm(false)
+		}
+	};
+  return (
+    <Container maxWidth="sm">
+	<AccountMenu onClick={handleOnClick} />
+	{openForm && <BasicForm />}
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          React Skills test
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
